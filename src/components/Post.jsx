3c1fad2c998c8bea192693fc094c5d655705ae4e
unknown <lebotelhon@gmail.com>
2022-07-10 22:55:59 -0300

    Desisti do Ignite, mto dificil

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,20 +1,34 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author, publishedAt }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
   return(
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar hasBorder={true} src="https://github.com/diego3g.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Diego Fernandes</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio ás 08:13" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime="2022-05-11 08:13:30">
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
